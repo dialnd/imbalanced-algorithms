c5d0e7c91ad32235c8b1c326d996012edba6d300
Reid Johnson <reid.johnson@gmail.com>
2022-01-28 23:03:00 -0800

    Update for sklearn 1.0.X

--- smote.py
+++ smote.py
@@ -3,10 +3,10 @@ from collections import Counter
 import numpy as np
 from sklearn.base import is_regressor
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble.forest import BaseForest
+from sklearn.ensemble._forest import BaseForest
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import normalize
-from sklearn.tree.tree import BaseDecisionTree
+from sklearn.tree import BaseDecisionTree
 from sklearn.utils import check_random_state
 from sklearn.utils import check_X_y
 #from sklearn.utils import shuffle
@@ -24,6 +24,7 @@ class SMOTE(object):
     ----------
     k_neighbors : int, optional (default=5)
         Number of nearest neighbors.
+
     random_state : int or None, optional (default=None)
         If int, random_state is the seed used by the random number generator.
         If None, the random number generator is the RandomState instance used
@@ -62,8 +63,9 @@ class SMOTE(object):
 
             # Find the NN for each sample.
             # Exclude the sample itself.
-            nn = self.neigh.kneighbors(self.X[j].reshape(1, -1),
-                                       return_distance=False)[:, 1:]
+            nn = self.neigh.kneighbors(
+                self.X[j].reshape(1, -1), return_distance=False
+            )[:, 1:]
             nn_index = np.random.choice(nn[0])
 
             dif = self.X[nn_index] - self.X[j]
@@ -104,25 +106,31 @@ class SMOTEBoost(AdaBoostClassifier):
     ----------
     n_samples : int, optional (default=100)
         Number of new synthetic samples per boosting step.
+
     k_neighbors : int, optional (default=5)
         Number of nearest neighbors.
+
     base_estimator : object, optional (default=DecisionTreeClassifier)
         The base estimator from which the boosted ensemble is built.
         Support for sample weighting is required, as well as proper `classes_`
         and `n_classes_` attributes.
+
     n_estimators : int, optional (default=50)
         The maximum number of estimators at which boosting is terminated.
         In case of perfect fit, the learning procedure is stopped early.
+
     learning_rate : float, optional (default=1.)
         Learning rate shrinks the contribution of each classifier by
         ``learning_rate``. There is a trade-off between ``learning_rate`` and
         ``n_estimators``.
+
     algorithm : {'SAMME', 'SAMME.R'}, optional (default='SAMME.R')
         If 'SAMME.R' then use the SAMME.R real boosting algorithm.
         ``base_estimator`` must support calculation of class probabilities.
         If 'SAMME' then use the SAMME discrete boosting algorithm.
         The SAMME.R algorithm typically converges faster than SAMME,
         achieving a lower test error with fewer boosting iterations.
+
     random_state : int or None, optional (default=None)
         If int, random_state is the seed used by the random number generator.
         If None, the random number generator is the RandomState instance used
@@ -136,25 +144,27 @@ class SMOTEBoost(AdaBoostClassifier):
            Knowledge Discovery (PKDD), 2003.
     """
 
-    def __init__(self,
-                 n_samples=100,
-                 k_neighbors=5,
-                 base_estimator=None,
-                 n_estimators=50,
-                 learning_rate=1.,
-                 algorithm='SAMME.R',
-                 random_state=None):
+    def __init__(
+        self,
+        n_samples=100,
+        k_neighbors=5,
+        base_estimator=None,
+        n_estimators=50,
+        learning_rate=1.,
+        algorithm="SAMME.R",
+        random_state=None
+    ):
 
         self.n_samples = n_samples
         self.algorithm = algorithm
-        self.smote = SMOTE(k_neighbors=k_neighbors,
-                           random_state=random_state)
+        self.smote = SMOTE(k_neighbors=k_neighbors, random_state=random_state)
 
         super(SMOTEBoost, self).__init__(
             base_estimator=base_estimator,
             n_estimators=n_estimators,
             learning_rate=learning_rate,
-            random_state=random_state)
+            random_state=random_state
+        )
 
     def fit(self, X, y, sample_weight=None, minority_target=None):
         """Build a boosted classifier/regressor from the training set (X, y),
@@ -167,12 +177,15 @@ class SMOTEBoost(AdaBoostClassifier):
             DOK, or LIL. COO, DOK, and LIL are converted to CSR. The dtype is
             forced to DTYPE from tree._tree if the base classifier of this
             ensemble weighted boosting classifier is a tree or forest.
+
         y : array-like of shape = [n_samples]
             The target values (class labels in classification, real numbers in
             regression).
+
         sample_weight : array-like of shape = [n_samples], optional
             Sample weights. If None, the sample weights are initialized to
             1 / n_samples.
+
         minority_target : int
             Minority class label.
 
@@ -187,25 +200,30 @@ class SMOTEBoost(AdaBoostClassifier):
         BaseWeightBoosting `fit` methods.
         """
         # Check that algorithm is supported.
-        if self.algorithm not in ('SAMME', 'SAMME.R'):
+        if self.algorithm not in ("SAMME", "SAMME.R"):
             raise ValueError("algorithm %s is not supported" % self.algorithm)
 
         # Check parameters.
         if self.learning_rate <= 0:
             raise ValueError("learning_rate must be greater than zero")
 
-        if (self.base_estimator is None or
-                isinstance(self.base_estimator, (BaseDecisionTree,
-                                                 BaseForest))):
+        if (self.base_estimator is None or isinstance(
+            self.base_estimator, (BaseDecisionTree, BaseForest)
+        )):
             DTYPE = np.float64  # from fast_dict.pxd
             dtype = DTYPE
-            accept_sparse = 'csc'
+            accept_sparse = "csc"
         else:
             dtype = None
-            accept_sparse = ['csr', 'csc']
+            accept_sparse = ["csr", "csc"]
 
-        X, y = check_X_y(X, y, accept_sparse=accept_sparse, dtype=dtype,
-                         y_numeric=is_regressor(self))
+        X, y = check_X_y(
+            X,
+            y,
+            accept_sparse=accept_sparse,
+            dtype=dtype,
+            y_numeric=is_regressor(self)
+        )
 
         if sample_weight is None:
             # Initialize weights to 1 / n_samples.
@@ -220,12 +238,12 @@ class SMOTEBoost(AdaBoostClassifier):
             if sample_weight.sum() <= 0:
                 raise ValueError(
                     "Attempting to fit with a non-positive "
-                    "weighted number of samples.")
+                    "weighted number of samples."
+                )
 
         if minority_target is None:
             # Determine the minority class label.
             stats_c_ = Counter(y)
-            maj_c_ = max(stats_c_, key=stats_c_.get)
             min_c_ = min(stats_c_, key=stats_c_.get)
             self.minority_target = min_c_
         else:
@@ -248,8 +266,11 @@ class SMOTEBoost(AdaBoostClassifier):
             if len(X_min) >= self.smote.k:
                 self.smote.fit(X_min)
                 X_syn = self.smote.sample(self.n_samples)
-                y_syn = np.full(X_syn.shape[0], fill_value=self.minority_target,
-                                dtype=np.int64)
+                y_syn = np.full(
+                    X_syn.shape[0],
+                    fill_value=self.minority_target,
+                    dtype=np.int64
+                )
 
                 # Normalize synthetic sample weights based on current training set.
                 sample_weight_syn = np.empty(X_syn.shape[0], dtype=np.float64)
@@ -260,20 +281,24 @@ class SMOTEBoost(AdaBoostClassifier):
                 y = np.append(y, y_syn)
 
                 # Combine the weights.
-                sample_weight = \
-                    np.append(sample_weight, sample_weight_syn).reshape(-1, 1)
-                sample_weight = \
-                    np.squeeze(normalize(sample_weight, axis=0, norm='l1'))
+                sample_weight = np.append(
+                    sample_weight, sample_weight_syn
+                ).reshape(-1, 1)
+                sample_weight = np.squeeze(
+                    normalize(sample_weight, axis=0, norm="l1")
+                )
 
-                # X, y, sample_weight = shuffle(X, y, sample_weight,
-                #                              random_state=random_state)
+                #X, y, sample_weight = shuffle(
+                #    X, y, sample_weight, random_state=random_state
+                #)
 
             # Boosting step.
             sample_weight, estimator_weight, estimator_error = self._boost(
                 iboost,
                 X, y,
                 sample_weight,
-                random_state)
+                random_state
+            )
 
             # Early termination.
             if sample_weight is None:

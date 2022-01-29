63401df9b92cf968ef461e3123e361766fddc048
Reid Johnson <reid.johnson@gmail.com>
2022-01-28 23:43:48 -0800

    Update for sklearn 1.0.X

--- ramo.py
+++ ramo.py
@@ -3,10 +3,10 @@ from collections import Counter
 import numpy as np
 from sklearn.base import is_regressor
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble.forest import BaseForest
+from sklearn.ensemble._forest import BaseForest
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import normalize
-from sklearn.tree.tree import BaseDecisionTree
+from sklearn.tree import BaseDecisionTree
 from sklearn.utils import check_random_state
 from sklearn.utils import check_X_y
 #from sklearn.utils import shuffle
@@ -23,19 +23,27 @@ class RankedMinorityOversampler(object):
     k_neighbors_1 : int, optional (default=5)
         Number of nearest neighbors used to adjust the sampling probability of
         the minority examples.
+
     k_neighbors_2 : int, optional (default=5)
         Number of nearest neighbors used to generate the synthetic data
         instances.
+
     alpha : float, optional (default=0.3)
         Scaling coefficient.
+
     random_state : int or None, optional (default=None)
         If int, random_state is the seed used by the random number generator.
         If None, the random number generator is the RandomState instance used
         by np.random.
     """
 
-    def __init__(self, k_neighbors_1=5, k_neighbors_2=5, alpha=0.3,
-                 random_state=None):
+    def __init__(
+        self,
+        k_neighbors_1=5,
+        k_neighbors_2=5,
+        alpha=0.3,
+        random_state=None,
+    ):
         self.k_neighbors_1 = k_neighbors_1
         self.k_neighbors_2 = k_neighbors_2
         self.alpha = alpha
@@ -64,8 +72,9 @@ class RankedMinorityOversampler(object):
 
             # Find the NN for each sample.
             # Exclude the sample itself.
-            nn = self.neigh_2.kneighbors(self.X_min[j].reshape(1, -1),
-                                         return_distance=False)[:, 1:]
+            nn = self.neigh_2.kneighbors(
+                self.X_min[j].reshape(1, -1), return_distance=False
+            )[:, 1:]
             nn_index = np.random.choice(nn[0])
 
             dif = self.X_min[nn_index] - self.X_min[j]
@@ -82,17 +91,19 @@ class RankedMinorityOversampler(object):
         ----------
         X : array-like, shape = [n_total_samples, n_features]
             Holds the majority and minority samples.
+
         y : array-like, shape = [n_total_samples]
             Holds the class targets for samples.
+
         sample_weight : array-like of shape = [n_samples], optional
             Sample weights multiplier. If None, the multiplier is 1.
+
         minority_target : int, optional (default=None)
             Minority class label.
         """
         if minority_target is None:
             # Determine the minority class label.
             stats_c_ = Counter(y)
-            maj_c_ = max(stats_c_, key=stats_c_.get)
             min_c_ = min(stats_c_, key=stats_c_.get)
             self.minority_target = min_c_
         else:
@@ -121,7 +132,7 @@ class RankedMinorityOversampler(object):
             self.r[i] = 1. / (1 + np.exp(-self.alpha * majority_neighbors))
 
         self.r = (self.r * sample_weight_min).reshape(1, -1)
-        self.r = np.squeeze(normalize(self.r, axis=1, norm='l1'))
+        self.r = np.squeeze(normalize(self.r, axis=1, norm="l1"))
 
         # Learn nearest neighbors.
         self.neigh_2 = NearestNeighbors(n_neighbors=self.k_neighbors_2 + 1)
@@ -144,31 +155,39 @@ class RAMOBoost(AdaBoostClassifier):
     ----------
     n_samples : int, optional (default=100)
         Number of new synthetic samples per boosting step.
+
     k_neighbors_1 : int, optional (default=5)
         Number of nearest neighbors used to adjust the sampling probability of
         the minority examples.
+
     k_neighbors_2 : int, optional (default=5)
         Number of nearest neighbors used to generate the synthetic data
         instances.
+
     alpha : float, optional (default=0.3)
         Scaling coefficient.
+
     base_estimator : object, optional (default=DecisionTreeClassifier)
         The base estimator from which the boosted ensemble is built.
         Support for sample weighting is required, as well as proper `classes_`
         and `n_classes_` attributes.
+
     n_estimators : int, optional (default=50)
         The maximum number of estimators at which boosting is terminated.
         In case of perfect fit, the learning procedure is stopped early.
+
     learning_rate : float, optional (default=1.)
         Learning rate shrinks the contribution of each classifier by
         ``learning_rate``. There is a trade-off between ``learning_rate`` and
         ``n_estimators``.
+
     algorithm : {'SAMME', 'SAMME.R'}, optional (default='SAMME.R')
         If 'SAMME.R' then use the SAMME.R real boosting algorithm.
         ``base_estimator`` must support calculation of class probabilities.
         If 'SAMME' then use the SAMME discrete boosting algorithm.
         The SAMME.R algorithm typically converges faster than SAMME,
         achieving a lower test error with fewer boosting iterations.
+
     random_state : int or None, optional (default=None)
         If int, random_state is the seed used by the random number generator.
         If None, the random number generator is the RandomState instance used
@@ -181,27 +200,31 @@ class RAMOBoost(AdaBoostClassifier):
            2010.
     """
 
-    def __init__(self,
-                 n_samples=100,
-                 k_neighbors_1=5,
-                 k_neighbors_2=5,
-                 alpha=0.3,
-                 base_estimator=None,
-                 n_estimators=50,
-                 learning_rate=1.,
-                 algorithm='SAMME.R',
-                 random_state=None):
+    def __init__(
+        self,
+        n_samples=100,
+        k_neighbors_1=5,
+        k_neighbors_2=5,
+        alpha=0.3,
+        base_estimator=None,
+        n_estimators=50,
+        learning_rate=1.,
+        algorithm="SAMME.R",
+        random_state=None,
+    ):
 
         self.n_samples = n_samples
         self.algorithm = algorithm
-        self.ramo = RankedMinorityOversampler(k_neighbors_1, k_neighbors_2,
-                                              alpha, random_state=random_state)
+        self.ramo = RankedMinorityOversampler(
+            k_neighbors_1, k_neighbors_2, alpha, random_state=random_state
+        )
 
         super(RAMOBoost, self).__init__(
             base_estimator=base_estimator,
             n_estimators=n_estimators,
             learning_rate=learning_rate,
-            random_state=random_state)
+            random_state=random_state,
+        )
 
     def fit(self, X, y, sample_weight=None, minority_target=None):
         """Build a boosted classifier/regressor from the training set (X, y),
@@ -234,25 +257,30 @@ class RAMOBoost(AdaBoostClassifier):
         BaseWeightBoosting `fit` methods.
         """
         # Check that algorithm is supported.
-        if self.algorithm not in ('SAMME', 'SAMME.R'):
+        if self.algorithm not in ("SAMME", "SAMME.R"):
             raise ValueError("algorithm %s is not supported" % self.algorithm)
 
         # Check parameters.
         if self.learning_rate <= 0:
             raise ValueError("learning_rate must be greater than zero")
 
-        if (self.base_estimator is None or
-                isinstance(self.base_estimator, (BaseDecisionTree,
-                                                 BaseForest))):
+        if (self.base_estimator is None or isinstance(
+            self.base_estimator, (BaseDecisionTree, BaseForest)
+        )):
             DTYPE = np.float64  # from fast_dict.pxd
             dtype = DTYPE
-            accept_sparse = 'csc'
+            accept_sparse = "csc"
         else:
             dtype = None
-            accept_sparse = ['csr', 'csc']
+            accept_sparse = ["csr", "csc"]
 
-        X, y = check_X_y(X, y, accept_sparse=accept_sparse, dtype=dtype,
-                         y_numeric=is_regressor(self))
+        X, y = check_X_y(
+            X,
+            y,
+            accept_sparse=accept_sparse,
+            dtype=dtype,
+            y_numeric=is_regressor(self)
+        )
 
         if sample_weight is None:
             # Initialize weights to 1 / n_samples.
@@ -267,12 +295,12 @@ class RAMOBoost(AdaBoostClassifier):
             if sample_weight.sum() <= 0:
                 raise ValueError(
                     "Attempting to fit with a non-positive "
-                    "weighted number of samples.")
+                    "weighted number of samples."
+                )
 
         if minority_target is None:
             # Determine the minority class label.
             stats_c_ = Counter(y)
-            maj_c_ = max(stats_c_, key=stats_c_.get)
             min_c_ = min(stats_c_, key=stats_c_.get)
             self.minority_target = min_c_
         else:
@@ -292,8 +320,9 @@ class RAMOBoost(AdaBoostClassifier):
             # RAMO step.
             self.ramo.fit(X, y, sample_weight=sample_weight)
             X_syn = self.ramo.sample(self.n_samples)
-            y_syn = np.full(X_syn.shape[0], fill_value=self.minority_target,
-                            dtype=np.int64)
+            y_syn = np.full(
+                X_syn.shape[0], fill_value=self.minority_target, dtype=np.int64
+            )
 
             # Combine the minority and majority class samples.
             X = np.vstack((X, X_syn))
@@ -304,10 +333,12 @@ class RAMOBoost(AdaBoostClassifier):
             sample_weight_syn[:] = 1. / X.shape[0]
 
             # Combine the weights.
-            sample_weight = \
-                np.append(sample_weight, sample_weight_syn).reshape(-1, 1)
-            sample_weight = \
-                np.squeeze(normalize(sample_weight, axis=0, norm='l1'))
+            sample_weight = np.append(
+                sample_weight, sample_weight_syn
+            ).reshape(-1, 1)
+            sample_weight = np.squeeze(
+                normalize(sample_weight, axis=0, norm="l1")
+            )
 
             # X, y, sample_weight = shuffle(X, y, sample_weight,
             #                              random_state=random_state)
@@ -315,9 +346,11 @@ class RAMOBoost(AdaBoostClassifier):
             # Boosting step.
             sample_weight, estimator_weight, estimator_error = self._boost(
                 iboost,
-                X, y,
+                X,
+                y,
                 sample_weight,
-                random_state)
+                random_state,
+            )
 
             # Early termination.
             if sample_weight is None:
--- rus.py
+++ rus.py
@@ -3,9 +3,9 @@ from collections import Counter
 import numpy as np
 from sklearn.base import is_regressor
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble.forest import BaseForest
+from sklearn.ensemble._forest import BaseForest
 from sklearn.preprocessing import normalize
-from sklearn.tree.tree import BaseDecisionTree
+from sklearn.tree import BaseDecisionTree
 from sklearn.utils import check_random_state
 from sklearn.utils import check_X_y
 #from sklearn.utils import shuffle
@@ -21,17 +21,23 @@ class RandomUnderSampler(object):
     ----------
     with_replacement : bool, optional (default=True)
         Undersample with replacement.
+
     return_indices : bool, optional (default=False)
         Whether or not to return the indices of the samples randomly selected
         from the majority class.
+
     random_state : int or None, optional (default=None)
         If int, random_state is the seed used by the random number generator.
         If None, the random number generator is the RandomState instance used
         by np.random.
     """
 
-    def __init__(self, with_replacement=True, return_indices=False,
-                 random_state=None):
+    def __init__(
+        self,
+        with_replacement=True,
+        return_indices=False,
+        random_state=None,
+    ):
         self.return_indices = return_indices
         self.with_replacement = with_replacement
         self.random_state = random_state
@@ -54,9 +60,11 @@ class RandomUnderSampler(object):
         if self.n_majority_samples <= n_samples:
             n_samples = self.n_majority_samples
 
-        idx = np.random.choice(self.n_majority_samples,
-                               size=self.n_majority_samples - n_samples,
-                               replace=self.with_replacement)
+        idx = np.random.choice(
+            self.n_majority_samples,
+            size=self.n_majority_samples - n_samples,
+            replace=self.with_replacement,
+        )
 
         if self.return_indices:
             return (self.X[idx], idx)
@@ -91,27 +99,34 @@ class RUSBoost(AdaBoostClassifier):
     ----------
     n_samples : int, optional (default=100)
         Number of new synthetic samples per boosting step.
+
     min_ratio : float (default=1.0)
         Minimum ratio of majority to minority class samples to generate.
+
     with_replacement : bool, optional (default=True)
         Undersample with replacement.
+
     base_estimator : object, optional (default=DecisionTreeClassifier)
         The base estimator from which the boosted ensemble is built.
         Support for sample weighting is required, as well as proper `classes_`
         and `n_classes_` attributes.
+
     n_estimators : int, optional (default=50)
         The maximum number of estimators at which boosting is terminated.
         In case of perfect fit, the learning procedure is stopped early.
+
     learning_rate : float, optional (default=1.)
         Learning rate shrinks the contribution of each classifier by
         ``learning_rate``. There is a trade-off between ``learning_rate`` and
         ``n_estimators``.
+
     algorithm : {'SAMME', 'SAMME.R'}, optional (default='SAMME.R')
         If 'SAMME.R' then use the SAMME.R real boosting algorithm.
         ``base_estimator`` must support calculation of class probabilities.
         If 'SAMME' then use the SAMME discrete boosting algorithm.
         The SAMME.R algorithm typically converges faster than SAMME,
         achieving a lower test error with fewer boosting iterations.
+
     random_state : int or None, optional (default=None)
         If int, random_state is the seed used by the random number generator.
         If None, the random number generator is the RandomState instance used
@@ -125,28 +140,33 @@ class RUSBoost(AdaBoostClassifier):
            (ICPR), 2008.
     """
 
-    def __init__(self,
-                 n_samples=100,
-                 min_ratio=1.0,
-                 with_replacement=True,
-                 base_estimator=None,
-                 n_estimators=50,
-                 learning_rate=1.,
-                 algorithm='SAMME.R',
-                 random_state=None):
+    def __init__(
+        self,
+        n_samples=100,
+        min_ratio=1.0,
+        with_replacement=True,
+        base_estimator=None,
+        n_estimators=50,
+        learning_rate=1.,
+        algorithm="SAMME.R",
+        random_state=None,
+    ):
 
         self.n_samples = n_samples
         self.min_ratio = min_ratio
         self.algorithm = algorithm
-        self.rus = RandomUnderSampler(with_replacement=with_replacement,
-                                      return_indices=True,
-                                      random_state=random_state)
+        self.rus = RandomUnderSampler(
+            with_replacement=with_replacement,
+            return_indices=True,
+            random_state=random_state,
+        )
 
         super(RUSBoost, self).__init__(
             base_estimator=base_estimator,
             n_estimators=n_estimators,
             learning_rate=learning_rate,
-            random_state=random_state)
+            random_state=random_state,
+        )
 
     def fit(self, X, y, sample_weight=None, minority_target=None):
         """Build a boosted classifier/regressor from the training set (X, y),
@@ -159,12 +179,15 @@ class RUSBoost(AdaBoostClassifier):
             DOK, or LIL. COO, DOK, and LIL are converted to CSR. The dtype is
             forced to DTYPE from tree._tree if the base classifier of this
             ensemble weighted boosting classifier is a tree or forest.
+
         y : array-like of shape = [n_samples]
             The target values (class labels in classification, real numbers in
             regression).
+
         sample_weight : array-like of shape = [n_samples], optional
             Sample weights. If None, the sample weights are initialized to
             1 / n_samples.
+
         minority_target : int
             Minority class label.
 
@@ -179,25 +202,30 @@ class RUSBoost(AdaBoostClassifier):
         BaseWeightBoosting `fit` methods.
         """
         # Check that algorithm is supported.
-        if self.algorithm not in ('SAMME', 'SAMME.R'):
+        if self.algorithm not in ("SAMME", "SAMME.R"):
             raise ValueError("algorithm %s is not supported" % self.algorithm)
 
         # Check parameters.
         if self.learning_rate <= 0:
             raise ValueError("learning_rate must be greater than zero")
 
-        if (self.base_estimator is None or
-                isinstance(self.base_estimator, (BaseDecisionTree,
-                                                 BaseForest))):
+        if (self.base_estimator is None or isinstance(
+            self.base_estimator, (BaseDecisionTree, BaseForest)
+        )):
             DTYPE = np.float64  # from fast_dict.pxd
             dtype = DTYPE
-            accept_sparse = 'csc'
+            accept_sparse = "csc"
         else:
             dtype = None
-            accept_sparse = ['csr', 'csc']
+            accept_sparse = ["csr", "csc"]
 
-        X, y = check_X_y(X, y, accept_sparse=accept_sparse, dtype=dtype,
-                         y_numeric=is_regressor(self))
+        X, y = check_X_y(
+            X,
+            y,
+            accept_sparse=accept_sparse,
+            dtype=dtype,
+            y_numeric=is_regressor(self),
+        )
 
         if sample_weight is None:
             # Initialize weights to 1 / n_samples.
@@ -212,12 +240,12 @@ class RUSBoost(AdaBoostClassifier):
             if sample_weight.sum() <= 0:
                 raise ValueError(
                     "Attempting to fit with a non-positive "
-                    "weighted number of samples.")
+                    "weighted number of samples."
+                )
 
         if minority_target is None:
             # Determine the minority class label.
             stats_c_ = Counter(y)
-            maj_c_ = max(stats_c_, key=stats_c_.get)
             min_c_ = min(stats_c_, key=stats_c_.get)
             self.minority_target = min_c_
         else:
@@ -248,30 +276,37 @@ class RUSBoost(AdaBoostClassifier):
             y_rus = y[np.where(y != self.minority_target)][X_idx]
             y_min = y[np.where(y == self.minority_target)]
 
-            sample_weight_rus = \
-                sample_weight[np.where(y != self.minority_target)][X_idx]
-            sample_weight_min = \
-                sample_weight[np.where(y == self.minority_target)]
+            sample_weight_rus = sample_weight[
+                np.where(y != self.minority_target)
+            ][X_idx]
+            sample_weight_min = sample_weight[
+                np.where(y == self.minority_target)
+            ]
 
             # Combine the minority and majority class samples.
             X = np.vstack((X_rus, X_min))
             y = np.append(y_rus, y_min)
 
             # Combine the weights.
-            sample_weight = \
-                np.append(sample_weight_rus, sample_weight_min).reshape(-1, 1)
-            sample_weight = \
-                np.squeeze(normalize(sample_weight, axis=0, norm='l1'))
+            sample_weight = np.append(
+                sample_weight_rus, sample_weight_min
+            ).reshape(-1, 1)
+            sample_weight = np.squeeze(
+                normalize(sample_weight, axis=0, norm="l1")
+            )
 
-            # X, y, sample_weight = shuffle(X, y, sample_weight,
-            #                              random_state=random_state)
+            #X, y, sample_weight = shuffle(
+            #    X, y, sample_weight, random_state=random_state
+            #)
 
             # Boosting step.
             sample_weight, estimator_weight, estimator_error = self._boost(
                 iboost,
-                X, y,
+                X,
+                y,
                 sample_weight,
-                random_state)
+                random_state,
+            )
 
             # Early termination.
             if sample_weight is None:
